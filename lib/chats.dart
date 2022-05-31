0a2d3256d674751e28ef193c36097baf5cbe62cf
FlutLab.io <>
2022-05-31 05:44:12 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -22,7 +22,7 @@ class Chats extends StatelessWidget {
             message('Maritta Dwi', 'Okey fir', '10.19', Icons.done_all, Colors.blue, NetworkImage('https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2021/08/31/3237294347.jpg')),
             message('Kelas TI-2D', 'Anda: iy rek', '11.40', Icons.done, Colors.grey, NetworkImage('https://cdn.popbela.com/content-images/post/20190519/img-242e1b2552582f651aee24c72e5e65e4_750x500.jpg')),
             message('Aditya Wiguna', 'Pcc', 'kemarin', Icons.done_all, Colors.blue, NetworkImage('https://cdn.kibrispdr.org/data/foto-bayangan-orang-23.jpg')),
-            message('Khusus Info 2D-TI', 'Adelina: Selamat malam teman2, maaf kemarin rabu', 'kemarin', Icons.done, Colors.grey, NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0_npgzbcjH_BvhqLeniK1kz2WOwNtjtAgcs8XQuu31qdInRQl948vXZUw8m2ntoEGrgA&usqp=CAU')),
+            message('Khusus Info 2D-TI', 'Adelina: Selamat malam teman', 'kemarin', Icons.done, Colors.grey, NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0_npgzbcjH_BvhqLeniK1kz2WOwNtjtAgcs8XQuu31qdInRQl948vXZUw8m2ntoEGrgA&usqp=CAU')),
             message('Maritta Dwi', 'Okey fir', '10.19', Icons.done_all, Colors.blue, NetworkImage('https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2021/08/31/3237294347.jpg')),
             message('Maritta Dwi', 'Okey fir', '10.19', Icons.done_all, Colors.blue, NetworkImage('https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2021/08/31/3237294347.jpg')),
             message('Maritta Dwi', 'Okey fir', '10.19', Icons.done_all, Colors.blue, NetworkImage('https://assets.promediateknologi.com/crop/0x0:0x0/x/photo/2021/08/31/3237294347.jpg')),
