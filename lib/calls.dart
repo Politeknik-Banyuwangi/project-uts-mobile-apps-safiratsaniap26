3c2afc11e01e2b16155f2fc18a30996e3a7a0311
FlutLab.io <>
2022-05-31 04:46:58 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -18,11 +18,11 @@ class Calls extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            tlephone('maritta', Icons.call_missed, Colors.red, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
-            tlephone('maritta', Icons.call_made, Colors.red, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
             tlephone('maritta', Icons.call_received, Colors.red, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
-            tlephone('maritta', Icons.call_made, Colors.red, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
-            tlephone('maritta', Icons.call_made, Colors.red, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
+            tlephone('maritta', Icons.call_made, Colors.green, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
+            tlephone('maritta', Icons.call_received, Colors.red, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
+            tlephone('maritta', Icons.call_made, Colors.green, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
+            tlephone('maritta', Icons.call_made, Colors.green, 'mei 20, 14:54', NetworkImage('https://lzd-img-global.slatic.net/g/p/24d8f239fab8b5880bd7b8434fe1c4f3.jpg_720x720q80.jpg_.webp')),
             // Masukkan Coding disini jangan rubah lainnya
             //
             //
